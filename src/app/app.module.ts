8a8c6d8c47782e6df1e5b86fce0201621d11f5ef
vishal <vishalwaykar85@gmail.com>
2021-06-25 17:31:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
